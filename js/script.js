13f0ae30fb3cf299cf588fc6f1c85417b36be7ab
Pratham Garg <prathamgarg2010@gmail.com>
2021-02-26 12:12:57 +0530

    Updated

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$("#academics").click(function(){
+		window.location.href = "https://drive.google.com/drive/folders/1eZYSuGoUI-PfGVA6Vy3UizOZZKokDgch?usp=sharing";
+	});
+	$("#comics").click(function(){
+		window.location.href = "https://drive.google.com/drive/folders/1jFz7XZi3kJM-KXjWwmSfCXXc2fmvjK1N?usp=sharing";
+	});
+	$("#encyclopedia").click(function(){
+		window.location.href = "https://drive.google.com/drive/folders/1GZVpVjcCPSQm496r5Z8Ga73fRBn4uuTm?usp=sharing";
+	});
+	$("#fictional").click(function(){
+		window.location.href = "https://drive.google.com/drive/folders/1HOPETo-oKyxi-_YZrORIkuXMOYb0jQA0?usp=sharing";
+	});
+	$("#non-fictional").click(function(){
+		window.location.href = "https://drive.google.com/drive/folders/1aw-zpSUqblnHCODWsVr8hraMMxBbwoOv?usp=sharing";
+	});
+	$("#periodicals").click(function(){
+		// window.location.href = "https://drive.google.com/drive/folders/1eZYSuGoUI-PfGVA6Vy3UizOZZKokDgch?usp=sharing";
+	});
+});
