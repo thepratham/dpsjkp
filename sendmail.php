587e87b1edb7a9f6e58e54c1287f6503da300e36
Pratham Garg <prathamgarg2010@gmail.com>
2021-02-26 18:00:19 +0530

    Updated

---
+++ sendmail.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST["submit"])){
+	$name = $_POST["fd_name"];
+	$email = $_POST["fd_email"];
+	$phone = $_POST["fd_phone"];
+	$msg = $_POST["feedback"];
+
+	// use wordwrap() if lines are longer than 70 characters
+	$msg = wordwrap($msg,70);
+
+	// send email
+	mail("prathamgarg2010@gmail.com","Digital Library - Feedback",$msg);
+}
+
+?>
