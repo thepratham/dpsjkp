3b95414a7a70cd8931aade1e9e620fa4253a6877
Pratham Garg <prathamgarg2010@gmail.com>
2021-02-27 14:07:29 +0530

    Updated

--- js/script.js
+++ js/script.js
@@ -21,18 +21,18 @@ $(document).ready(function(){
 
 	// periodical icons
 	$("#jagran").click(function(){
-		// window.location.href = "https://drive.google.com/drive/folders/1eZYSuGoUI-PfGVA6Vy3UizOZZKokDgch?usp=sharing";
+		window.location.href = "https://www.jagran.com/";
 	});
 	$("#amarujala").click(function(){
-		// window.location.href = "https://drive.google.com/drive/folders/1jFz7XZi3kJM-KXjWwmSfCXXc2fmvjK1N?usp=sharing";
+		window.location.href = "https://epaper.amarujala.com/";
 	});
 	$("#hindustantimes").click(function(){
-		// window.location.href = "https://drive.google.com/drive/folders/1GZVpVjcCPSQm496r5Z8Ga73fRBn4uuTm?usp=sharing";
+		window.location.href = "https://epaper.hindustantimes.com/Home/mIndex?htepaper=mobile&tstmp=1204202004";
 	});
 	$("#champak").click(function(){
-		// window.location.href = "https://drive.google.com/drive/folders/1HOPETo-oKyxi-_YZrORIkuXMOYb0jQA0?usp=sharing";
+		window.location.href = "http://www.champak.in/";
 	});
 	$("#jansatta").click(function(){
-		// window.location.href = "https://drive.google.com/drive/folders/1aw-zpSUqblnHCODWsVr8hraMMxBbwoOv?usp=sharing";
+		window.location.href = "https://epaper.jansatta.com/t/23814/?s=%E0%A4%B2%E0%A4%96%E0%A4%A8%E0%A4%8A";
 	});
 });
